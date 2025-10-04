e4640780ceea0ac1ab37275606ad5857b187b62c
Son Dao <udemydaohsn@gmail.com>
2025-10-03 23:34:19 -0700

    create useTradingViewWidget and reusable TradingViewWidget

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,19 @@
+import TradingViewWidget from "@/components/TradingViewWidget";
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants";
+
 const Page = () => {
-  return <div className="flex min-h-screen home-wrapper">Home</div>;
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+  return (
+    <div className="md:col-span-1 xl:col-span-1">
+      <TradingViewWidget
+        title="Market Overview"
+        scriptUrl={`${scriptUrl}market-overview.js`}
+        config={MARKET_OVERVIEW_WIDGET_CONFIG}
+        className="custom-chart"
+        height={600}
+      />
+    </div>
+  );
 };
 
 export default Page;
--- src/components/UserDropDown.tsx
+++ src/components/UserDropDown.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,17 +10,20 @@ import {
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { LogOut } from "lucide-react";
+import NavItems from "./NavItems";
 
 const UserDropDown = () => {
   const router = useRouter();
 
-  const handleSignout = async () => {
+  const handleSignOut = async () => {
     router.push("/sign-in");
   };
 
   const user = {
     name: "John",
-    email: "contact@John.com",
+    email: "contact@john.com",
   };
   return (
     <DropdownMenu>
@@ -32,7 +34,7 @@ const UserDropDown = () => {
           className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
         >
           <Avatar className="h-8 w-8">
-            {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
+            <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
               {user.name[0]}
             </AvatarFallback>
@@ -44,13 +46,35 @@ const UserDropDown = () => {
           </div>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      <DropdownMenuContent className="text-gray-400">
+        <DropdownMenuLabel>
+          <div className="flex relative items-center gap-3 py-2">
+            <Avatar className="h-10 w-10">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                {user.name[0]}
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <span className="text-base font-medium text-gray-400">
+                {user.name}
+              </span>
+              <span className="text-sm text-gray-500">{user.email}</span>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator className="bg-gray-600" />
+        <DropdownMenuItem
+          onClick={handleSignOut}
+          className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer"
+        >
+          <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
+          Logout
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
+        <nav className="sm:hidden">
+          <NavItems />
+        </nav>
       </DropdownMenuContent>
     </DropdownMenu>
   );
